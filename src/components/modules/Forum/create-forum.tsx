a9b9e5fafb7f6b392bfe0f67ee7dee92219096f0
ikbar <108569380+ghifarikbar@users.noreply.github.com>
2024-06-25 11:17:10 +0700

    update forum input image not found original name

--- src/components/modules/Forum/create-forum.tsx
+++ src/components/modules/Forum/create-forum.tsx
@@ -160,20 +160,6 @@ export default function CreateForum() {
                     onChange={handleImagePreview}
                   />
                   <label htmlFor="image" className="flex cursor-pointer items-center text-white">
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      className="mr-2 h-6 w-6 text-red-500"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      stroke="currentColor"
-                    >
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        strokeWidth={2}
-                        d="M12 6v6m0 0v6m0-6h6m-6 0H6"
-                      />
-                    </svg>
                     <span className="text-red-500">* </span> Upload Gambar
                   </label>
                   {errors.image && <span className="error">{errors.image.message}</span>}
