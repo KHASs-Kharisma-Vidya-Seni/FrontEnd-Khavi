7a9de3197b83002742429b4f4405120c7c8e481d
ikbar <kroniikbar@gmail.com>
2024-06-28 14:20:45 +0700

    fix: user not login face scanner

--- src/pages/FaceScanner.tsx
+++ src/pages/FaceScanner.tsx
@@ -1,8 +1,14 @@
 import React from "react";
 import AnimationPage from "@/components/AnimationPage";
 import Scanner from "@/components/modules/scanner/scanner-analisis";
+import { useAuth } from "@/hooks/use-auth";
 
 const FaceScanner: React.FC = () => {
+  const { currentUser } = useAuth();
+
+  if(!currentUser || currentUser === null) {
+    return <div className="flex h-screen w-screen items-center justify-center">Please login use Face Scanner</div>
+  }
   return (
     <AnimationPage>
       <div className="container mx-auto py-10">
